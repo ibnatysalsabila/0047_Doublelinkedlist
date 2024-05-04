df76f117266c0b729852d090b7d014a8bfbc05ff
ibnatysalsabila <salsajpr01@gmail.com>
2024-05-04 18:50:47 +0700

    menambahkan perulangan node  rollNo

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -97,5 +97,12 @@ void deleteNode()
     current = START;
     previous = NULL;
 
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+
 
 
