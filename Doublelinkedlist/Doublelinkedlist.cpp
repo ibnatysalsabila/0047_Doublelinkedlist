22882f68e8ecbb479d8f55a80157a553f2094a82
ibnatysalsabila <salsajpr01@gmail.com>
2024-05-04 18:54:31 +0700

    menghubungkan node sebelumnya dengan node sesudahnya

--- Doublelinkedlist/Doublelinkedlist.cpp
+++ Doublelinkedlist/Doublelinkedlist.cpp
@@ -118,5 +118,14 @@ void deleteNode()
         }
     }
 
+    else
+    {
+        previous->next = current->next;
+        if (current->next != NULL)
+        {
+            current->next->prev = previous;
+        }
+    }
+
 
 
